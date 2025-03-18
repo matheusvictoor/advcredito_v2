bb19c5d597f33ae4f585392f0a6d87e10737a043
Matheus Victor <matheusvictor.dev@gmail.com>
2025-03-18 15:08:07 -0300

    chore: remove unnecessary Next.js build artifacts and add .dockerignore

--- apps/dashboard/.next/server/app/manifest.webmanifest/route.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/manifest.webmanifest/route.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/d0430_next_0af7e3._.js");
-runtime.loadChunk("server/chunks/[root of the server]__ffaefd._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.2_@babel+core@7.26.7_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/apps/dashboard/app/manifest--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
